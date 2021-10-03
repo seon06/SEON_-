344b595877c917727a0ecdd89b2b9f9db5e5360f
seon06 <daareum.seon@icloud.com>
2021-10-03 20:30:27 +0900

    OPTIFINE 창 추가

---
+++ main.py
@@ -0,0 +1,193 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.ttk import Separator
+import os
+import sys
+import requests
+from shutil import rmtree as rmt
+import shutil
+from subprocess import check_output as sp_c_o
+from subprocess import STDOUT as STDOUT
+from webbrowser import open as wb_o
+from requests import get
+from PIL import Image,ImageTk
+import pyglet
+import pickle
+import threading
+
+font_name = ('Jua-Regular')
+p = str(os.path.dirname(os.path.realpath(__file__))).replace('\\','/')
+
+with open('data.seon_simple_installer','rb') as f:
+    data = pickle.load(f)
+    f.close()
+    #print(data)
+
+texts = {
+    '0':{
+        'kor':'<int>초 후 업데이트를 체크합니다.'
+        },
+    '1':{
+        'kor':'''SEON 간편설치기 이용 가이드
+
+1. 인터넷에 연결 되어있는지 확인해 주세요.
+
+2.Java가 설치되어있는지 확인해주세요.
+(설치 되어있지 않다면 "Java 설치" 버튼을 눌려주세요.)
+
+3.이 프로그램은 .minecraft, mods, shaders 폴더를 수정합니다.
+
+위 내용을 모두 확인 하였으면 "다음" 버튼을 눌려주세요.
+
+Copyright 2021. seon06 all rights reserved.
+'''
+        },
+    '2':{
+        'kor':'''SEON 간편설치기
+
+OPTIFINE을 설치 하실려면 "OPTIFINE" 을
+MOD를 설치 하실려면 "MOD"을 클릭해주세요.
+
+*OPTIFINE: 마인크래프트 최적화 프로그램, 쉐이더 적용가능
+OPTIFINE 선택시 OPTIFINE, SHADER 설치 가능
+
+*MOD:
+MOD 선택시 FORGE, MOD, OPTIFINE 설치 가능
+
+*OPTIFINE과 MOD를 함께 적용 할려면 MOD를 선택해 주세요.
+'''
+        }
+    }
+
+version = '0.0.1'
+
+def uc():
+    global root
+    root.destroy()
+    main_page()
+def _gtou(url):
+    url = url.replace('https://drive.google.com/file/d/','').replace('/view','')
+    return 'https://drive.google.com/u/0/uc?id='+str(url)
+def uce():
+    global root,p
+    if True:
+        try:
+            r = requests.get('https://api.github.com/repos/seon06/SEON_SIMPLE_INSTALLER_FOR_MC/releases/latest')
+            r = r.json()['name'].split(' ')[2].replace('v','')
+            print(r)
+            if r == version:
+                messagebox.Message(title='업데이트 완료',message='최신 버전이에요.').show()
+            else:
+                messagebox.Message(title='업데이트 발견',message='앗! 최신 업데이트를 발견했어요.\n아직 자동 업데이트 기능이 없으니\n업데이트를 하시고 싶으시면 GITHUB에서 다운로드 해주세요!').show()
+            root.destroy()
+            main_page()
+        except Exception as e:
+            print(e)
+            messagebox.showerror(title='!',message='업데이트를 불러오는데 실패했어요\n업데이트를 취소하고 설치를 진행합니다.')
+            root.destroy()
+            main_page()
+        
+    else:
+        root.destroy()
+        main_page()
+def loading():
+    def ac(v):
+        global una
+        una.config(text=texts['0']['kor'].replace('<int>',str(v)))
+        if v == 0:
+            uce()
+    global una,root
+    root = Tk()
+    button_i = Image.open('%s/system/img/button3.png'%p)
+    button_image = ImageTk.PhotoImage(button_i.resize((150,50), Image.ANTIALIAS))
+    root.title('SEON 간편설치기')
+    root.resizable(False, False)
+    root.geometry('700x400')
+    root.config(bg='white')
+    F = Frame(root, bg='white')
+    Label(F,text='SEON 간편 설치기',font=('Jua',20),bg='white',fg='blue').pack()
+    Separator(F,orient='horizontal').pack(fill='both')
+    una = Label(F,text=texts['0']['kor'].replace('<int>','3'),bg='yellow',font=('Jua',15))
+    una.pack(pady=50)
+    Button(F,text='자동 업데이트 건너뀌기',command=uc,image=button_image,bg='white',highlightthickness = 0, borderwidth=0,compound='center',font=('Jua',11)).pack(side='bottom',pady=20)
+    F.pack(fill='both',padx=80,expand=True)
+    root.after(1000, lambda: ac(2))
+    root.after(2000, lambda: ac(1))
+    root.after(3000, lambda: ac(0))
+    root.mainloop()
+def main_page():
+    def n():
+        root.destroy()
+        select_sm()
+    root = Tk()
+    root.title('SEON 간편설치기')
+    root.resizable(False, False)
+    button_i = Image.open('%s/system/img/button3.png'%p)
+    button_image = ImageTk.PhotoImage(button_i.resize((150,50), Image.ANTIALIAS))
+    root.geometry('700x500')
+    root.config(bg='white')
+    F = Frame(root,bg='white')
+    Label(F,text='SEON 간편 설치기',font=('Jua',20),bg='white',fg='blue').pack()
+    Separator(F,orient='horizontal').pack(fill='both')
+    a = Text(F,bg='white',fg='gray',highlightthickness = 0, borderwidth=0)
+    a.insert(END,texts['1']['kor'])
+    a.config(state=DISABLED)
+    a.pack(anchor=CENTER,pady=20)
+    f = Frame(root,bg='white')
+    Button(f,text='Java 설치',command= lambda: wb_o('https://java.com/ko/download/ie_manual.jsp?locale=ko'),image=button_image,bg='white',highlightthickness = 0, borderwidth=0,compound='center',font=('Jua',11)).pack(side='left')
+    Button(f,text='다음',command=n,image=button_image,bg='white',highlightthickness = 0, borderwidth=0,compound='center',font=('Jua',11)).pack(side='right')
+    
+    F.pack(fill='x',padx=80)
+    f.pack()
+    root.mainloop()
+def select_sm():
+    root = Tk()
+    root.title('SEON 간편설치기')
+    root.resizable(False, False)
+    def o():
+        root.destroy()
+        select_optifine()
+    button_i = Image.open('%s/system/img/button3.png'%p)
+    button_image = ImageTk.PhotoImage(button_i.resize((150,50), Image.ANTIALIAS))
+    root.geometry('700x500')
+    root.config(bg='white')
+
+    F = Frame(root,bg='white')
+    f = Frame(root,bg='white')
+    Label(F,text='SEON 간편 설치기',font=('Jua',20),bg='white',fg='blue').pack()
+    Separator(F,orient='horizontal').pack(fill='both')
+    a = Text(F,bg='white',fg='gray',highlightthickness = 0, borderwidth=0)
+    a.insert(END,texts['2']['kor'])
+    a.config(state=DISABLED)
+    a.pack(anchor=CENTER,pady=20)
+    Button(f,text='OPTIFINE',command=o,image=button_image, bg='white',highlightthickness = 0, borderwidth=0,compound='center',font=('Jua',11)).pack(side='left')
+    Button(f,text='MOD',image=button_image, bg='white',highlightthickness = 0, borderwidth=0,compound='center',font=('Jua',11)).pack(side='right')
+    
+    F.pack(fill='x',padx=80)
+    f.pack()
+    root.mainloop()
+
+def select_optifine():
+    root = Tk()
+    root.title('SEON 간편설치기')
+    root.resizable(False, False)
+    button_i = Image.open('%s/system/img/button3.png'%p)
+    button_image = ImageTk.PhotoImage(button_i.resize((150,50), Image.ANTIALIAS))
+    root.geometry('700x500')
+    root.config(bg='white')
+
+    F = Frame(root,bg='white')
+    Label(F,text='SEON 간편 설치기',font=('Jua',20),bg='white',fg='blue').pack()
+    Separator(F,orient='horizontal').pack(fill='both')
+    L = Listbox(F,selectmode='single',font=('Jua',15))
+    for i in data['download']['mods']:
+        if i['name'] == 'optifine':
+            f = i['files']
+    for i in f:
+        L.insert(len(f)-(f.index(i)+1),str(i['mc_version'] if i['mc_version'] != '1.17' else '1.17 ')+' '+i['version'])
+    L.pack(fill='x')
+    
+    F.pack(fill='x',padx=80)
+    root.mainloop()
+pyglet.font.add_file('%s/system/Jua-Regular.ttf'%p)
+loading()
---
+++ make_file.py
@@ -0,0 +1,165 @@
+#https://drive.google.com/u/0/uc?id=11P-7o-csJIiQnGxz17ScqqsO85Vq6LCA&export=download
+options = {
+    'update':'https://drive.google.com/file/d/11P-7o-csJIiQnGxz17ScqqsO85Vq6LCA/view',
+    'download':{
+            'mods':[
+                {
+                    'name':"vic's_modern_warfare_mod",
+                    'files':[
+                        {
+                            'mc_version':'1.12.2',
+                            'link':'https://media.forgecdn.net/files/2934/384/mw_2.0-0.4.4.6_mc1.12.2.jar',
+                            'forge_version':'1.12.2',
+                            'kor':'모던워페어'
+                            }
+                        ]
+                    },
+             
+                {
+                    'name':'pixelmon_reforged',
+                    'files':[
+                        {
+                            'mc_version':'1.12.2',
+                            'link':'https://download.nodecdn.net/containers/reforged/universal/release/Pixelmon-1.12.2-8.3.0-universal.jar',
+                            'version':'8.3.0-universal',
+                            'forge_version':'1.12.2-14.23.5.2854',
+                            'kor':'픽셀몬(포켓몬) 리포지드'
+                            }
+                        ]
+                    },
+
+                {
+                    'name':'the_twilight_forest',
+                    'files':[
+                        {
+                            'mc_version':'1.16.5',
+                            'link':'https://media.forgecdn.net/files/3413/560/twilightforest-1.16.5-4.0.546-universal.jar',
+                            'forge_version':'1.16.5',
+                            'version':'4.0.546 BETA'
+                            },
+                        {
+                            'mc_version':'1.12.2',
+                            'link':'https://media.forgecdn.net/files/3051/450/twilightforest-1.12.2-3.11.1021-universal.jar',
+                            'version':'3.11.1021',
+                            'forge_version':'1.12.2',
+                            'kor':'황혼의 숲'
+                         }
+                        ]
+                    },
+                {
+                    'name':'optifine',
+                    'files':[
+                        {
+                            'mc_version':'1.17.1',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.17.1_HD_U_G9.jar&x=9b57b49ce1f3069d011955ce222fbf72',
+                            'version':'HD_U_G9'
+                            },
+                        {
+                            'mc_version':'1.17',
+                            'link':'https://optifine.net/downloadx?f=preview_OptiFine_1.17_HD_U_G9_pre26.jar&x=9b57b49ce1f3069d011955ce222fbf72',
+                            'version':'HD_U_G9_pre26'
+                            },
+                        {
+                            'mc_version':'1.16.5',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.16.5_HD_U_G8.jar&x=b49c4c89b97f73bc328bc443e0a85ea9',
+                            'version':'HD_U_G8'
+                            },
+                        {
+                            'mc_version':'1.16.3',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.16.3_HD_U_G5.jar&x=e15ca5181dcefc10a0f07c1c7e3cb2ae',
+                            'version':'HD_U_G5'
+                            },
+                        {
+                            'mc_version':'1.16.2',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.16.2_HD_U_G5.jar&x=b49c4c89b97f73bc328bc443e0a85ea9',
+                            'version':'HD_U_G5'
+                            },
+                        {
+                            'mc_version':'1.15.2',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.15.2_HD_U_G6.jar&x=b49c4c89b97f73bc328bc443e0a85ea9',
+                            'version':'HD_U_G6'
+                            },
+                        {
+                            'mc_version':'1.14.4',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.14.4_HD_U_G5.jar&x=b49c4c89b97f73bc328bc443e0a85ea9',
+                            'version':'HD_U_G5'
+                            },
+                        {
+                            'mc_version':'1.14.2',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.14.2_HD_U_F1.jar&x=ed6159cb5d117cd04c73a64e45279b40',
+                            'version':'HD_U_F1'
+                            },
+                        {
+                            'mc_version':'1.13.2',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.13.2_HD_U_G5.jar&x=ed6159cb5d117cd04c73a64e45279b40',
+                            'version':'HD_U_G5'
+                            },
+                        {
+                            'mc_version':'1.12.2',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.12.2_HD_U_G5.jar&x=18bc2ff8e0e3e106515b85e32af4d3a2',
+                            'version':'HD_U_G5'
+                            },
+                        {
+                            'mc_version':'1.12.2',
+                            'link':'https://optifine.net/downloadx?f=OptiFine_1.12.2_HD_U_F5.jar&x=ddd51da0e231521cdbd5680de0c9031e',
+                            'version':'HD_U_F5'
+                            }
+                        ]
+                    }
+                ],
+            'shaders':[
+                {
+                    'name':'SEUS',
+                    'mc_version':'all',
+                    'link':'https://sonicether.com/shaders/download/renewed-v1-0-1/download.php',
+                    'version':'Renewed-v1.0.1',
+                    'optifine_version':'HD_U_F5',
+                    'eula':'https://sonicether.com/shaders/download/renewed-v1-0-1/#eula'
+                    }
+                ]
+            },
+    'texts':{
+        '0':{
+        'kor':'<int>초 후 업데이트를 체크합니다.'
+            },
+        '1':{
+        'kor':'''SEON 간편설치기 이용 가이드
+
+1. 인터넷에 연결 되어있는지 확인해 주세요.
+
+2.Java가 설치되어있는지 확인해주세요.
+(설치 되어있지 않다면 "Java 설치" 버튼을 눌려주세요.)
+
+3.이 프로그램은 .minecraft, mods, shaders 폴더를 수정합니다.
+
+위 내용을 모두 확인 하였으면 "다음" 버튼을 눌려주세요.
+
+Copyright 2021. seon06 all rights reserved.
+'''
+            },
+        '2':{
+        'kor':'''SEON 간편설치기
+
+OPTIFINE을 설치 하실려면 "OPTIFINE" 을
+MOD를 설치 하실려면 "MOD"을 클릭해주세요.
+
+*OPTIFINE: 마인크래프트 최적화 프로그램, 쉐이더 적용가능
+OPTIFINE 선택시 OPTIFINE, SHADER 설치 가능
+
+*MOD:
+MOD 선택시 FORGE, MOD, OPTIFINE 설치 가능
+
+*OPTIFINE과 MOD를 함께 적용 할려면 MOD를 선택해 주세요.
+'''
+            }
+        }
+    }
+import pickle
+with open('data.seon_simple_installer','wb') as f:
+    pickle.dump(options,f)
+    f.close()
+with open('data.seon_simple_installer','rb') as f:
+    optionsL = pickle.load(f)
+    f.close()
+print(optionsL)
+
